0cc87630548ec214efa0712910997c795f1673f7
Gabriel Alves <alves.gabriel7@gmail.com>
2024-06-04 06:11:44 -0300

    Lendo bytes com FileStream

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,10 +1,44 @@
 ﻿using ByteBankIO;
+using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
+        try
+        {
+            string enderecoDoArquivo = "C:\\Users\\alves\\Downloads\\CsharpArquivos-main\\CsharpArquivos-main\\contas.txt";
+            var numeroDeBytesLidos = -1;
 
-        Console.ReadLine();
+            
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024];
+
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+
+            while (numeroDeBytesLidos!= 0)
+            {
+                fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+           
+            Console.ReadLine();
+        }
+        catch (Exception e)
+        {
+            throw e;
+        }
+       
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        foreach (var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }
     }
 }
