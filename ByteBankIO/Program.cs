98baa783f694f75ab9842935dbd2198d260d5f42
Gabriel Alves <alves.gabriel7@gmail.com>
2024-06-05 06:30:13 -0300

    Usando leitura de arquivo com StreamReader

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -7,7 +7,36 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        Console.ReadLine();
+        try
+        {
+            string enderecoDoArquivo = "C:\\Users\\alves\\Downloads\\CsharpArquivos-main\\CsharpArquivos-main\\contas.txt";
+
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                var leitor = new StreamReader(fluxoDeArquivo);
+
+                //var linha = leitor.ReadLine();
+
+                //var arquivoCompleto = leitor.ReadToEnd();
+
+                //var numero = leitor.Read();
+
+                while (!leitor.EndOfStream)
+                {
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+        
     }
 
 
