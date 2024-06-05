f37baba552d09072876e4412d5929c5eab4b0615
Gabriel Alves <alves.gabriel7@gmail.com>
2024-06-05 06:07:46 -0300

    Organizando o projeto utilizando partial

---
+++ ByteBankIO/1_LidandoComFileStreamDiretamente.cs
@@ -0,0 +1,49 @@
+﻿using ByteBankIO;
+using System;
+using System.IO;
+using System.Text;
+
+partial class Program
+{
+    static void LidandoComFileStramDiretamente()
+    {
+        try
+        {
+            string enderecoDoArquivo = "C:\\Users\\alves\\Downloads\\CsharpArquivos-main\\CsharpArquivos-main\\contas.txt";
+            var numeroDeBytesLidos = -1;
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+
+                var buffer = new byte[1024];
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+
+                    Console.WriteLine($" Bytes lidos: {numeroDeBytesLidos}");
+
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+                }
+                fluxoDoArquivo.Close();
+                Console.ReadLine();
+            }
+
+        }
+        catch (Exception e)
+        {
+            throw e;
+        }
+
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
+
+        Console.Write(texto);
+    }
+}
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -3,49 +3,12 @@ using System;
 using System.IO;
 using System.Text;
 
-class Program
+partial class Program
 {
     static void Main(string[] args)
     {
-        try
-        {
-            string enderecoDoArquivo = "C:\\Users\\alves\\Downloads\\CsharpArquivos-main\\CsharpArquivos-main\\contas.txt";
-            var numeroDeBytesLidos = -1;
-
-            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-            {
-
-                var buffer = new byte[1024];
-
-                while (numeroDeBytesLidos != 0)
-                {
-                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
-
-                    Console.WriteLine($" Bytes lidos: {numeroDeBytesLidos}");
-
-                    EscreverBuffer(buffer, numeroDeBytesLidos);
-                }
-                fluxoDoArquivo.Close();
-                Console.ReadLine();
-            }
-
-        }
-        catch (Exception e)
-        {
-            throw e;
-        }
-       
+        Console.ReadLine();
     }
 
-    static void EscreverBuffer(byte[] buffer, int bytesLidos)
-    {
 
-        var utf8 = new UTF8Encoding();
-
-        var texto = utf8.GetString(buffer, 0, bytesLidos);
-
-
-
-        Console.Write(texto);
-    }
 }
