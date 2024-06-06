b17823b24ff60d9a8515348a5bf90f57cc106634
Gabriel Alves <alves.gabriel7@gmail.com>
2024-06-06 05:36:57 -0300

    Montando uma conta corrente apartir do arquivo txt

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -24,7 +24,10 @@ partial class Program
                 while (!leitor.EndOfStream)
                 {
                     var linha = leitor.ReadLine();
-                    Console.WriteLine(linha);
+                    var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                    var msg = $"Conta Numero {contaCorrente.Numero}, ag {contaCorrente.Agencia}, saldo {contaCorrente.Saldo}";
+                    Console.WriteLine(msg);
                 }
 
             }
@@ -37,6 +40,37 @@ partial class Program
             throw;
         }
         
+    }
+
+    static ContaCorrente ConverterStringParaContaCorrente(string linha)
+    {
+        try
+        {
+            var campos = linha.Split(',');
+
+            var agencia = campos[0];
+            var numero = campos[1];
+            var saldo = campos[2].Replace('.', ',');
+            var nomeTitular = campos[3];
+
+            var agenciaComInt = int.Parse(agencia);
+            var numeroComInt = int.Parse(numero);
+            var saldoComDouble = double.Parse(saldo);
+
+            var titular = new Cliente();
+            titular.Nome = nomeTitular;
+
+            var resultado = new ContaCorrente(agenciaComInt, numeroComInt);
+            resultado.Depositar(saldoComDouble);
+            resultado.Titular = titular;
+
+            return resultado;
+        }
+        catch (Exception e)
+        {
+
+            throw e;
+        }
     }
 
 
